8e10da622265f52ee1051ed9a4f0485aeb4ec5a0
Louis Knolle <knollelw@gmail.com>
2022-04-04 22:22:01 -0400

    add forms and Javascript

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-  
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    const nameInput = $("input#name").val();
+    const food = $("#food").val();
+    const dog = $("input:radio[name=dog]:checked").val();
+    const dob = $("#born").val();
+    const favoriteColor = $("#color").val();
+  });
 });
